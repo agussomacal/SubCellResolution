Bug in grad solved. ELVIRA for not 0 1 situations. Smoothness by gradient. Plot curve.

src/lib/CellCreators/CurveCellCreators/CurveCellCreatorBase.py
@@ -62,13 +62,14 @@ class CurveCellCreatorBase(CellCreatorBase):
             coords=coords, independent_axis=independent_axis, average_values=average_values,
             smoothness_index=smoothness_index, indexer=indexer, cells=cells, stencil=stencil,
         )
-        for curve in self.create_curves(average_values, indexer, cells, coords, smoothness_index,
-                                        independent_axis, stencil, regular_opposite_cells):
-            yield CellCurveBase(
-                coords=coords,
-                curve=curve,
-                regular_opposite_cells=regular_opposite_cells,
-                dependent_axis=1 - independent_axis)
+        if len(regular_opposite_cells) == 2:
+            for curve in self.create_curves(average_values, indexer, cells, coords, smoothness_index,
+                                            independent_axis, stencil, regular_opposite_cells):
+                yield CellCurveBase(
+                    coords=coords,
+                    curve=curve,
+                    regular_opposite_cells=regular_opposite_cells,
+                    dependent_axis=1 - independent_axis)
 
     def create_curves(self, average_values: np.ndarray, indexer: ArrayIndexerNd, cells: Dict[str, CellBase],
                       coords: CellCoords, smoothness_index: np.ndarray, independent_axis: int,

src/lib/CellCreators/CurveCellCreators/ELVIRACellCreator.py
@@ -16,15 +16,16 @@ class ELVIRACurveCellCreator(CurveCellCreatorBase):
     def create_curves(self, average_values: np.ndarray, indexer: ArrayIndexerNd, cells: Dict[str, CellBase],
                       coords: CellCoords, smoothness_index: np.ndarray, independent_axis: int,
                       stencil: Stencil, regular_opposite_cells: Tuple) -> Generator[CurveBase, None, None]:
-        stencil_values = stencil.values.sum(axis=1 - independent_axis)
+        value_up = regular_opposite_cells[1].evaluate(coords.coords)
+        value_down = regular_opposite_cells[0].evaluate(coords.coords)
+        # if the values are not 0 or 1
+        min_value = np.min((value_up, value_down))
+        stencil_values = stencil.values.sum(axis=1 - independent_axis) - 3 * min_value
 
         point_x = coords[independent_axis] + 0.5
         point_y = coords[1 - independent_axis] - 1.0
         # which side the integral has to be done, is 0 below the curve or is 1?
-        try:
-            jump = regular_opposite_cells[1].evaluate(coords.coords) - regular_opposite_cells[0].evaluate(coords.coords)
-        except:
-            regular_opposite_cells[1].evaluate(coords.coords)
+        jump = value_up - value_down
         if jump < 0:
             point_y += stencil_values[1]
         else:
@@ -35,6 +36,6 @@ class ELVIRACurveCellCreator(CurveCellCreatorBase):
                       (stencil_values[2] - stencil_values[0]) / 2]:
             yield CurvePolynomial(
                 polynomial=Polynomial([point_y - slope * point_x, slope]),
-                value_up=regular_opposite_cells[1].evaluate(coords.coords),
-                value_down=regular_opposite_cells[0].evaluate(coords.coords)
+                value_up=value_up,
+                value_down=value_down
             )

src/lib/CellCreators/CurveCellCreators/RegularCellsSearchers.py
@@ -28,7 +28,7 @@ def get_regular_opposite_cell_coords_by_direction(coords: CellCoords, cells: Dic
     singular_cells = {coords.tuple}
     for sign in [1, -1]:
         for i in np.arange(start, np.shape(average_values)[0]):
-            coords_i = 1 * coords.coords + i * direction * sign
+            coords_i = 1 * coords.coords + 0.5 + i * direction * sign
             coords_i = np.array(coords_i, dtype=int)
             if acceptance_criterion(coords_i):
                 regular_opposite_cells.append(tuple(coords_i))
@@ -76,6 +76,25 @@ def get_opposite_cells_by_smoothness_threshold(coords: CellCoords, cells: Dict[T
     return tuple([cells[tuple(indexer[o])] for o in regular_opposite_cell_coords])
 
 
+def get_opposite_cells_by_relative_smoothness(coords: CellCoords, cells: Dict[Tuple[int], CellBase],
+                                              independent_axis: int,
+                                              average_values: np.ndarray, smoothness_index: np.ndarray,
+                                              indexer: ArrayIndexerNd,
+                                              threshold=0.5, **kwargs):
+    regular_opposite_cell_coords, _ = get_regular_opposite_cell_coords_by_direction(
+        coords=coords, cells=cells, average_values=average_values, indexer=indexer,
+        direction=np.array([0, 1])[[independent_axis, 1 - independent_axis]],
+        acceptance_criterion=lambda coords_i: indexer[coords_i] in cells.keys() and
+                                              (cells[indexer[coords_i]].CELL_TYPE == REGULAR_CELL_TYPE) and
+                                              smoothness_index[indexer[coords_i]] <= threshold * smoothness_index[
+                                                  indexer[coords.tuple]],
+        start=2)
+
+    # order from down to up given dependant axis.
+    regular_opposite_cell_coords = sorted(regular_opposite_cell_coords, key=lambda c: c[1 - independent_axis])
+    return tuple([cells[tuple(indexer[o])] for o in regular_opposite_cell_coords])
+
+
 # def get_smooth_opposite_cells(coords: CellCoords, dependent_axis: int, average_values: np.ndarray,
 #                               smoothness_index, indexer: ArrayIndexerNd,
 #                               cells: Dict[Tuple[int], CellBase], stencil, **kwargs):
@@ -109,15 +128,15 @@ def get_opposite_cells_by_grad(coords: CellCoords, cells: Dict[Tuple[int], CellB
         acceptance_criterion=
         lambda coords_i: indexer[coords_i] in cells.keys() and
                          (cells[indexer[coords_i]].CELL_TYPE == REGULAR_CELL_TYPE) and
-                         (smoothness_index[indexer[coords_i]] < smoothness_index[indexer[coords.tuple]]),
+                         (smoothness_index[indexer[coords_i]] < 0.5 * smoothness_index[indexer[coords.tuple]]),
         start=2)
 
-    regular_opposite_cell_coords, _ = get_regular_opposite_cell_coords_by_direction(
-        coords=coords, cells=cells, average_values=average_values, indexer=indexer,
-        direction=np.array([0, 1])[[independent_axis, 1 - independent_axis]],
-        acceptance_criterion=lambda coords_i: indexer[coords_i] in cells.keys() and
-                                              smoothness_index[indexer[coords_i]] <= threshold,
-        start=2)
+    # regular_opposite_cell_coords, _ = get_regular_opposite_cell_coords_by_direction(
+    #     coords=coords, cells=cells, average_values=average_values, indexer=indexer,
+    #     direction=np.array([0, 1])[[independent_axis, 1 - independent_axis]],
+    #     acceptance_criterion=lambda coords_i: indexer[coords_i] in cells.keys() and
+    #                                           smoothness_index[indexer[coords_i]] <= threshold,
+    #     start=2)
 
     # sc = sorted(stencil_coords, key=lambda c: smoothness_index[indexer[c]])
     # first_neighbour = sc.pop(0)

src/lib/CellOrientators.py
@@ -14,14 +14,14 @@ def approximate_gradient_by(average_values, method="scharr", normalize=False):
     :return:
     """
     if method == "scharr":
-        scharr_gx = np.array(
+        scharr_gy = np.array(
             [[3, 0, -3],
              [10, 0, -10],
              [3, 0, -3]])
-        scharr_gy = np.array(
+        scharr_gx = np.array(
             [[3, 10, 3],
              [0, 0, 0],
-             [-3, -100, -3]])
+             [-3, -10, -3]])
         g = np.array([np.sum(average_values * scharr_gx), np.sum(average_values * scharr_gy)])
     elif method == "sobel":
         raise Exception("Not implemented method {}.".format(method))