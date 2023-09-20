c33c484a4d9ee2604ce11cadabc3dbeb0f9ae089
somacal <agus.somacal@gmail.com>
2023-09-20 20:23:00 +0200

    run_all_experiments.py

--- src/experiments/subcell_paper/run_all_experiments.py
+++ src/experiments/subcell_paper/run_all_experiments.py
@@ -1,11 +1,20 @@
-from PerplexityLab.miscellaneous import timeit
+import os
+import subprocess
+from time import time
 
+os.chdir("./../../")
+print(os.getcwd())
+dtime = dict()
 for experiment_name in ["ex_aero", "ex_binary_images", "ex_refinement", "ex_regular", "ex_scheme"]:
     try:
         print("========= ========= ========= ========= =========")
         print(f"         Running {experiment_name}              ")
         print("========= ========= ========= ========= =========")
-        with timeit(f"Time for {experiment_name}"):
-            exec(open(experiment_name + ".py").read())
+        t0 = time()
+        process = subprocess.Popen("python experiments/subcell_paper/" + experiment_name + ".py", shell=True)
+        process.wait()
+        dtime[experiment_name] = time() - t0
     except:
         print("Failed to run ", experiment_name)
+
+print(dtime)
