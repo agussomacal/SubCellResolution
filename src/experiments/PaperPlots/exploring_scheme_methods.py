Finite volumes with rotation field plus Zalesak test some bugs fixed.

src/experiments/PaperPlots/exploring_scheme_methods.py
@@ -73,38 +73,35 @@ def get_velocity_field(velocity, num_cells_per_dim, center=np.zeros(2), angular_
     velocity:
     rot_velocity: num_cells_per_dim x num_cells_per_dim x 2
     """
+    h = 1.0 / num_cells_per_dim
     if angular_velocity != 0:
-        h = 1.0 / num_cells_per_dim
         xy = h * (1 / 2 + np.array(np.meshgrid(range(num_cells_per_dim), range(num_cells_per_dim), indexing="ij")))
         r = (xy - np.array(center)[:, np.newaxis, np.newaxis])  # radius
         # rotation of 90º angle
         r = r[[1, 0]]  # reflection around 45º line
         r[0] = -r[0]  # reflection around x=0
-        rot_velocity = np.transpose(r, axes=(1, 2, 0)) * angular_velocity
+        new_angular_velocity = angular_velocity / np.max(np.abs(r))
+        rot_velocity = np.transpose(r, axes=(1, 2, 0)) * new_angular_velocity
     else:
+        new_angular_velocity = 0
         rot_velocity = 0
-    return np.array(velocity), rot_velocity
+    return np.array(velocity), rot_velocity, new_angular_velocity * h
 
 
 # ========== ========== Experiment definitions ========== ========== #
-def get_relative_angular_velocity(num_cells_per_dim, angular_velocity):
-    return np.arctan((1 / num_cells_per_dim) / (num_cells_per_dim / 2)) * angular_velocity
-
-
 def get_velocity4experiments(num_cells_per_dim, velocity, angular_velocity):
-    # center = 0.5 * np.ones(2) + np.array([0, 5.0 / 3 / num_cells_per_dim]) if angular_velocity != 0 else np.zeros(2)
-    # angular_velocity = get_relative_angular_velocity(num_cells_per_dim, angular_velocity)
     center = np.ones(2) / 2
-    velocity, rot_velocity = get_velocity_field(velocity, num_cells_per_dim, center, angular_velocity)
-    return velocity, rot_velocity, center
+    velocity, rot_velocity, new_angular_velocity = get_velocity_field(velocity, num_cells_per_dim, center,
+                                                                      angular_velocity)
+    return velocity, rot_velocity, center, new_angular_velocity
 
 
 def calculate_true_solution(image, num_cells_per_dim, velocity, ntimes, angular_velocity, SAVE_EACH):
     image = load_image(image)
     pixels_per_cell = np.array(np.shape(image)) / num_cells_per_dim
 
-    angular_velocity = get_relative_angular_velocity(num_cells_per_dim, angular_velocity) * pixels_per_cell[0]
-    velocity, _, _ = get_velocity4experiments(num_cells_per_dim, velocity, angular_velocity)
+    # angular_velocity = get_relative_angular_velocity(num_cells_per_dim, angular_velocity) * pixels_per_cell[0]
+    velocity, _, _, new_angular_velocity = get_velocity4experiments(num_cells_per_dim, velocity, angular_velocity)
     velocity_in_pixels = np.array(pixels_per_cell * np.array(velocity), dtype=int)
     assert np.all(velocity_in_pixels == pixels_per_cell * np.array(velocity))
 
@@ -116,13 +113,13 @@ def calculate_true_solution(image, num_cells_per_dim, velocity, ntimes, angular_
             true_reconstruction.append(image.copy())
         true_solution.append(calculate_averages_from_image(image, num_cells_per_dim))
         if not np.any(velocity):
-            image = ndimage.rotate(original_image, 180 * angular_velocity * (i + 1) / np.pi, mode="mirror",
+            image = ndimage.rotate(original_image, 180 * new_angular_velocity * (i + 1) / np.pi, mode="mirror",
                                    reshape=False)
-        elif angular_velocity == 0:
+        elif new_angular_velocity == 0:
             image = np.roll(original_image, velocity_in_pixels * (i + 1))
         else:
             image = np.roll(image, velocity_in_pixels / 2)
-            image = ndimage.rotate(image, 180 * angular_velocity / np.pi, mode="mirror", reshape=False)
+            image = ndimage.rotate(image, 180 * new_angular_velocity / np.pi, mode="mirror", reshape=False)
             image = np.roll(image, velocity_in_pixels / 2)
         # threshold to binarize again
         image[image < 0.5] = 0
@@ -147,7 +144,7 @@ def fit_model(subcell_reconstruction):
 
         # pixels_per_cell = np.array(np.shape(image_array)) / num_cells_per_dim
         # angular_velocity = get_relative_angular_velocity(num_cells_per_dim, angular_velocity)*pixels_per_cell[0]
-        velocity, rot_velocity, _ = get_velocity4experiments(num_cells_per_dim, velocity, angular_velocity)
+        velocity, rot_velocity, _, _ = get_velocity4experiments(num_cells_per_dim, velocity, angular_velocity)
         velocity = rot_velocity + velocity
         # finite volume solver evolution
         t0 = time.time()

src/experiments/PaperPlots/paper_scheme_plot.py
@@ -16,14 +16,18 @@ from experiments.global_params import cpink, corange, cblue, cgreen, runsinfo, c
     RESOLUTION_FACTOR, num_cores, running_in, only_create_preimage_data, image_format
 from experiments.PaperPlots.models2compare import upwind
 
-SAVE_EACH = 1
+ntimes = 120 if running_in in ["server", "local-power"] else 20
+ntimes = 60 if running_in in ["server", "local-power"] else 20
+# ntimes = 2*SAVE_EACH+1
+SAVE_EACH = 20
+angular_velocity = 2 / SAVE_EACH
 num_cells_per_dim = [15, 30, 60]
-num_cells_per_dim = [60]
+num_cells_per_dim = [30]
 
 
 def zalesak_notched_circle(num_pixels=1680):
-    radius = num_pixels / 4
-    rectangle_width = radius / 3
+    radius = num_pixels / 3
+    rectangle_width = radius / 3  # / 3
     rectangle_y_shift = rectangle_width
     rectangle_height = rectangle_width * 4
     center = num_pixels // 2 * np.ones(2)
@@ -95,7 +99,7 @@ if __name__ == "__main__":
     lab.define_new_block_of_functions(
         "ground_truth",
         calculate_true_solution,
-        recalculate=False
+        recalculate=True
     )
 
     lab.define_new_block_of_functions(
@@ -103,14 +107,13 @@ if __name__ == "__main__":
         *map(fit_model, [
             aero_qelvira_vertex,
             elvira,
-            elvira_w_oriented,
-            quadratic_aero,
-            upwind,
+            # elvira_w_oriented,
+            # quadratic_aero,
+            # upwind,
         ]),
-        recalculate=False
+        recalculate=True
     )
 
-    ntimes = 120 if running_in in ["server", "local-power"] else 20
     # lab.execute(
     #     data_manager,
     #     num_cores=num_cores,
@@ -137,7 +140,7 @@ if __name__ == "__main__":
         refinement=[1],
         ntimes=[ntimes],
         velocity=[(0, 0)],
-        angular_velocity=[0.25],
+        angular_velocity=[angular_velocity],
         num_cells_per_dim=num_cells_per_dim,  # 60
         noise=[0],
         image=[
@@ -146,11 +149,11 @@ if __name__ == "__main__":
             "zalesak_notched_circle.jpg",
         ],
         reconstruction_factor=[RESOLUTION_FACTOR],
-        SAVE_EACH=[8],
+        SAVE_EACH=[SAVE_EACH],
     )
     print(set(data_manager["models"]))
 
-    ntimes = 120
+    # ntimes = 120
     # ---------------- Paper plots ---------------- #
     generic_plot(data_manager,
                  name="ReconstructionErrorInTimex",
@@ -158,7 +161,7 @@ if __name__ == "__main__":
                  path=config.subcell_paper_figures_path,
                  x="times", y="scheme_error", label="method",
                  plot_by=["num_cells_per_dim", "image", "angular_velocity"],
-                 times=lambda ntimes: np.arange(0, ntimes, SAVE_EACH) + 1,
+                 times=lambda ntimes, SAVE_EACH: np.arange(0, ntimes, SAVE_EACH) + 1,
                  scheme_error=scheme_reconstruction_error,
                  plot_func=NamedPartial(
                      sns.lineplot,
@@ -186,39 +189,35 @@ if __name__ == "__main__":
                                                        extra_x_left=0.125, extra_x_right=0.075),
                  )
 
-    # for angular_velocity in [0.25, 0]:
-    #     for i in range(ntimes-1):
-    #         plot_time_i(
-    #             data_manager,
-    #             folder=f"sol_angvel{angular_velocity}",
-    #             name=f"Solution{i}",
-    #             i=i,
-    #             alpha=0.5, alpha_true_image=0.5, difference=False, plot_curve=True,
-    #             plot_curve_winner=False,
-    #             plot_vh_classification=True, plot_singular_cells=True, cmap="viridis",
-    #             cmap_true_image="Greys_r", draw_mesh=True,
-    #             trim=((0, 1), (0, 1)),
-    #             angular_velocity=angular_velocity,
-    #             numbers_on=True, vmin=None, vmax=None, labels=True,
-    #             plot_by=["num_cells_per_dim", "image", "velocity", "angular_velocity", "models"]
-    #         )
+    for i in range(ntimes // SAVE_EACH):
+        plot_reconstruction_time_i(
+            data_manager,
+            folder_by=["num_cells_per_dim", "angular_velocity", "models", ],
+            name=f"Reconstruction{i}",
+            i=i,
+            alpha=0.5, alpha_true_image=0.5, difference=False, plot_curve=True,
+            plot_curve_winner=False,
+            plot_vh_classification=True, plot_singular_cells=True, cmap="viridis",
+            cmap_true_image="Greys_r", draw_mesh=True,
+            trim=((0, 1), (0, 1)),
+            numbers_on=True, vmin=None, vmax=None, labels=True,
+            plot_by=["num_cells_per_dim", "angular_velocity", "image", "velocity", "models"]
+        )
 
-    for angular_velocity in [0.25]:
-        for i in range(ntimes-1):
-            plot_reconstruction_time_i(
-                data_manager,
-                folder=f"angvel{angular_velocity}",
-                name=f"Reconstruction{i}",
-                i=i,
-                alpha=0.5, alpha_true_image=0.5, difference=False, plot_curve=True,
-                plot_curve_winner=False,
-                plot_vh_classification=True, plot_singular_cells=True, cmap="viridis",
-                cmap_true_image="Greys_r", draw_mesh=True,
-                trim=((0, 1), (0, 1)),
-                angular_velocity=angular_velocity,
-                numbers_on=True, vmin=None, vmax=None, labels=True,
-                plot_by=["num_cells_per_dim", "image", "velocity", "angular_velocity", "models"]
-            )
+    for i in range(ntimes // SAVE_EACH):
+        plot_time_i(
+            data_manager,
+            folder_by=["num_cells_per_dim", "angular_velocity", "models", ],
+            name=f"Solution{i}",
+            i=i,
+            alpha=0.5, alpha_true_image=0.5, difference=False, plot_curve=True,
+            plot_curve_winner=False,
+            plot_vh_classification=True, plot_singular_cells=True, cmap="viridis",
+            cmap_true_image="Greys_r", draw_mesh=True,
+            trim=((0, 1), (0, 1)),
+            numbers_on=True, vmin=None, vmax=None, labels=True,
+            plot_by=["num_cells_per_dim", "image", "velocity", "angular_velocity", "models"]
+        )
     # ========== =========== ========== =========== #
     #               Experiment Times                #
     # ========== =========== ========== =========== #

src/lib/SubCellReconstruction.py
@@ -214,7 +214,8 @@ class SubCellFlux:
 class SubCellReconstruction:
     def __init__(self, name, smoothness_calculator, cell_classifier: Callable = cell_classifier_by_smoothness,
                  reconstruction_error_measure=ReconstructionErrorMeasureBase,
-                 cell_creators: List[CellCreatorPipeline] = [], refinement: int = 1, obera_iterations=0):
+                 cell_creators: List[CellCreatorPipeline] = [], refinement: int = 1, obera_iterations=0,
+                 eps_complexity=0):
         self.name = name
         self.smoothness_calculator = smoothness_calculator
         self.cell_classifier = cell_classifier
@@ -225,6 +226,7 @@ class SubCellReconstruction:
         self.stencils = dict()
         self.resolution = None
         self.obera_iterations = obera_iterations
+        self.eps_complexity = eps_complexity  # penalize models with more complexity if reconstruction errors are not too different.
 
         self.times = defaultdict(ddf)
         self.obera_fevals = defaultdict(ddf)
@@ -269,6 +271,8 @@ class SubCellReconstruction:
                 damping=np.array([1, 1, 1e-4])) for coords in iterate_all(smoothness_index)}
 
             for i, cell_creator in enumerate(self.cell_creators):
+                eps_complexity = self.eps_complexity[i] if isinstance(self.eps_complexity, list) \
+                    else self.eps_complexity
                 for coords in cell_creator.cell_iterator(smoothness_index=smoothness_index,
                                                          reconstruction_error=reconstruction_error):
                     if i not in cell_classification[coords.tuple]:
@@ -340,7 +344,7 @@ class SubCellReconstruction:
                                         else:
                                             old_cell_reconstruction_error = reconstruction_error[coords.tuple]
 
-                                        if proposed_cell_reconstruction_error < old_cell_reconstruction_error:
+                                        if proposed_cell_reconstruction_error + eps_complexity < old_cell_reconstruction_error:
                                             reconstruction_error[coords.tuple] = proposed_cell_reconstruction_error
                                             self.cells[coords.tuple] = proposed_cell
                                             self.stencils[coords.tuple] = list(map(tuple, stencil.coords.tolist()))