fcea794981671395c39d869964de2f1ebed6db69
somacal <agus.somacal@gmail.com>
2026-06-18 14:56:44 +0200

    Rewritten experiments with new simplified perplexity lab features.

--- src/experiments/Refinement/ex_refinement_reconstruction.py
+++ src/experiments/Refinement/ex_refinement_reconstruction.py
@@ -1,13 +1,13 @@
-from email import iterators
+from pathlib import Path
 
-from experiments.OtherExperiments.SubcellExperiments.models2compare import quadratic
+from experiments.OtherExperiments.SubcellExperiments.models2compare import quadratic, aero_linear, elvira
 from experiments.Refinement.ex_refinement_config import experiment_path, curve_color, cmap_reconstruction, \
     cmap_true_image, fig_size
-from experiments.Refinement.ex_refinement_tools import ExperimentInfo, image_to_avg, do_reconstruction, \
-    fit_model
+from experiments.Refinement.ex_refinement_tools import image_to_avg, do_reconstruction, fit_model
 from experiments.tools import load_image
 from experiments.tools4binary_images import plot_reconstruction4img
-from perplexitylab.experiment_tools import experiment_iterator, concatenate_iterators
+from perplexitylab.experiment_tools import experiment_iterator, concatenate_iterators, define_default_constants, \
+    define_default_variables
 from perplexitylab.plot_tools import save_figure
 
 if __name__ == "__main__":
@@ -17,44 +17,64 @@ if __name__ == "__main__":
     recalculate_all = False
 
     # ---------- Experiment list ---------- #
-    common_iterators = {"num_cells_per_dim": [20], "image_name": ["batata.jpg"], "refinement": [1, 2]}
-    iterators = [
-        experiment_iterator(ExperimentInfo, sub_cell_model=quadratic, label="quadratic", recalculate=False)
-    ]
-    iterator = concatenate_iterators(*[it(**common_iterators) for it in iterators])
+    iterator_builder, info = experiment_iterator(
+        experiment_name=Path(__file__).stem,
+        constants=define_default_constants(sub_cell_model=None, label=None, angle_threshold=0, reconstruction_factor=1,
+                                           recalculate=recalculate_all),
+        variables=define_default_variables(
+            num_cells_per_dim=[20, 42],
+            image_name=["batata.jpg"],
+            refinement=[1, 2, 3]
+        ))
+
+
+    def identifier(info):
+        return f"Img{info.image_name.split('.')[0]}_{info.num_cells_per_dim}x{info.num_cells_per_dim}_{info.label}_Ref{info.refinement}"
+
+
+    iterators = concatenate_iterators(
+        iterator_builder(sub_cell_model=quadratic, label="AEROS quadratic", recalculate=False or recalculate_all),
+        iterator_builder(sub_cell_model=aero_linear, label="AEROS linear", recalculate=False or recalculate_all),
+        iterator_builder(sub_cell_model=elvira, label="ELVIRA", recalculate=False or recalculate_all),
+    )
 
     # ---------- Do experiments ---------- #
-    for experiment_info, other_variables in iterator():
-        if recalculate_all or experiment_info.recalculate:
-            print("----------------------------------")
-            print(experiment_info.identifier.replace("_", " "))
-            image = load_image(experiment_info.image_name)
-            avg_values = image_to_avg(experiment_info=experiment_info, image=image, noise=noise,
-                                      seed=seed)
-            model = fit_model(experiment_info=experiment_info, avg_values=avg_values)
-            reconstruction = do_reconstruction(experiment_info=experiment_info, image=image, model=model)
-
-            with save_figure(filename=experiment_info.identifier, path=experiment_path, figsize=fig_size,
-                             show=False) as (fig, ax):
-                plot_reconstruction4img(
-                    fig=fig, ax=ax,
-                    image=experiment_info.image_name,
-                    num_cells_per_dim=experiment_info.num_cells_per_dim,
-                    model=model,
-                    reconstruction=reconstruction,
-                    difference=False,
-                    plot_curve=True,
-                    plot_curve_winner=False,
-                    plot_vh_classification=False,
-                    plot_singular_cells=False,
-                    alpha_true_image=0.15,
-                    alpha=0,
-                    trim=((1, 1), (2, 2)),
-                    cmap=cmap_reconstruction,
-                    cmap_true_image=cmap_true_image,
-                    curve_color=curve_color,
-                    vmin=0, vmax=1,
-                    labels=False,
-                    draw_mesh=False,
-                    numbers_on=False,
-                )
+    for experiment_info in iterators():
+        print("----------------------------------")
+        print(identifier(experiment_info))
+        image = load_image(experiment_info.image_name)
+        avg_values = image_to_avg(num_cells_per_dim=experiment_info.num_cells_per_dim, image=image, noise=noise,
+                                  seed=seed)
+        hash_value = 42
+        hash_value, model = fit_model(hash_of_preprocess=hash_value, recalculate=experiment_info.recalculate,
+                                      sub_cell_model=experiment_info.sub_cell_model,
+                                      angle_threshold=experiment_info.angle_threshold,
+                                      refinement=experiment_info.refinement, avg_values=avg_values)
+        hash_value, reconstruction = do_reconstruction(hash_of_preprocess=hash_value,
+                                                       recalculate=experiment_info.recalculate,
+                                                       image=image, model=model,
+                                                       reconstruction_factor=experiment_info.reconstruction_factor)
+        with save_figure(filename=identifier(experiment_info), path=experiment_path, figsize=fig_size,
+                         show=False) as (fig, ax):
+            plot_reconstruction4img(
+                fig=fig, ax=ax,
+                image=experiment_info.image_name,
+                num_cells_per_dim=experiment_info.num_cells_per_dim,
+                model=model,
+                reconstruction=reconstruction,
+                difference=False,
+                plot_curve=True,
+                plot_curve_winner=False,
+                plot_vh_classification=False,
+                plot_singular_cells=False,
+                alpha_true_image=0.15,
+                alpha=0,
+                trim=((1, 1), (2, 2)),
+                cmap=cmap_reconstruction,
+                cmap_true_image=cmap_true_image,
+                curve_color=curve_color,
+                vmin=0, vmax=1,
+                labels=False,
+                draw_mesh=False,
+                numbers_on=False,
+            )
--- src/experiments/Refinement/ex_refinement_tools.py
+++ src/experiments/Refinement/ex_refinement_tools.py
@@ -9,58 +9,38 @@ from experiments.Refinement.ex_refinement_config import experiment_path
 from experiments.global_params import EVALUATIONS
 from experiments.tools import calculate_averages_from_image, load_image, reconstruct
 from lib.AuxiliaryStructures.Indexers import ArrayIndexerNd
-from perplexitylab.miscellaneous import check_do_save_or_load_experiment
+from perplexitylab.experiment_tools import perplexifier
 
 
-@dataclass
-class ExperimentInfo:
-    label: str
-    image_name: str
-    num_cells_per_dim: int
-    sub_cell_model: Callable
-    refinement: int = 1
-    reconstruction_factor: int = 1
-    angle_threshold: float = 0.
-    # color: Union[str, Tuple] = "black"
-    # linestyle: str = 'dashed'
-    # marker: str = '.'
-    recalculate: bool = False
-
-    @property
-    def identifier(self):
-        return f"Img{self.image_name.split('.')[0]}_{self.num_cells_per_dim}x{self.num_cells_per_dim}_{self.label}_Ref{self.refinement}"
+def single_experiment_vars_filter(*args, **kwargs):
+    return list(map(str, args)) + list(map(str, kwargs.values()))
 
 
-def single_experiment_vars_filter(experiment_info: ExperimentInfo, *args, **kwargs):
-    return (experiment_info.identifier,)
-
-
-def image_to_avg(experiment_info: ExperimentInfo, image, noise=0, seed=42):
-    avg_values = calculate_averages_from_image(image, experiment_info.num_cells_per_dim)
+def image_to_avg(num_cells_per_dim, image, noise=0, seed=42):
+    avg_values = calculate_averages_from_image(image, num_cells_per_dim)
     np.random.seed(seed)
     return avg_values + np.random.uniform(-noise, noise, size=avg_values.shape)
 
 
-@check_do_save_or_load_experiment(default_path=experiment_path, vars_filter=single_experiment_vars_filter)
-def fit_model(experiment_info: ExperimentInfo, avg_values):
-    model = experiment_info.sub_cell_model(refinement=experiment_info.refinement,
-                                           angle_threshold=experiment_info.angle_threshold)
+@perplexifier(default_path=experiment_path)
+def fit_model(sub_cell_model, angle_threshold, refinement, avg_values):
+    model = sub_cell_model(refinement=refinement, angle_threshold=angle_threshold)
 
     t0 = time.time()
     model.fit(average_values=avg_values, indexer=ArrayIndexerNd(avg_values, "cyclic"))
     t_fit = time.time() - t0
-    print("Time to fit model:", t_fit)
+    print("\n\tTime to fit model:", t_fit)
     return model
 
 
-@check_do_save_or_load_experiment(default_path=experiment_path, vars_filter=single_experiment_vars_filter,
-                                  saver=lambda data, filepath: plt.imsave(filepath, data),
-                                  loader=lambda filepath: load_image(filepath, other_path=""),
-                                  file_format="png")
-def do_reconstruction(experiment_info: ExperimentInfo, image, model):
+@perplexifier(default_path=experiment_path,
+              saver=lambda data, filepath: plt.imsave(filepath, data),
+              loader=lambda filepath: load_image(filepath, other_path=""),
+              file_format="png")
+def do_reconstruction(image, model, reconstruction_factor):
     t0 = time.time()
-    reconstruction = reconstruct(image, model.cells, model.resolution, experiment_info.reconstruction_factor,
+    reconstruction = reconstruct(image, model.cells, model.resolution, reconstruction_factor,
                                  do_evaluations=EVALUATIONS)
     t_reconstruct = time.time() - t0
-    print("Time to reconstruct:", t_reconstruct)
+    print("\n\tTime to reconstruct:", t_reconstruct)
     return reconstruction
